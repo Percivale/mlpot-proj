aab7a0af52eb80f2bfe0ce400ed51ece3415cbbd
Percivale <kaja.hiorth@gmail.com>
2023-02-15 15:41:14 +0200

    increased number of atoms in input files

--- src/python/lammps_utils.py
+++ src/python/lammps_utils.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+def read_lammpstrj(path):
+    f = open(path, 'r')
+
+    get_val = False
+    info = np.zeros(5)
+    idx = 0
+    for line in f:
+        if get_val:
+            try:
+                val = int(line)
+            except ValueError:
+                val = float(line)
+        if line[:4] == "ITEM":
+            get_val = True
+
+
+    f.close()
